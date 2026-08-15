04b1723d6add6acebb5e95af5432fa643260cb62
mayurpandit25 <panditmayur888@gmail.com>
2026-08-15 20:25:01 +0530

    updated

---
+++ terraform/modules/alb-controller/varsions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+
+    helm = {
+      source = "hashicorp/helm"
+    }
+  }
+}
