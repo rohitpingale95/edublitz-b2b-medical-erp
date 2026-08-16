bf92696d5de2d510d325447ca1cf367f6e509861
mayurpandit25 <panditmayur888@gmail.com>
2026-08-16 12:51:31 +0530

    jenkinsfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -271,10 +271,10 @@ provider "helm" {
 # ---------------------------------------------------------
 
 resource "helm_release" "aws_load_balancer_controller" {
-
   name       = "aws-load-balancer-controller"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
+  version    = "1.8.2"
 
   namespace = "kube-system"
 
